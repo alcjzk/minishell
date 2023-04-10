6b1443b4bd9967830641c336c123afeaf2a24e70
Dan Palmer <dpalmer@student.hive.fi>
2023-04-10 11:00:19 +0300

    i need some coffee. 3rd try is a charm

--- src/file_exp.c
+++ src/file_exp.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 11:18:42 by dpalmer           #+#    #+#             */
-/*   Updated: 2023/04/10 10:56:18 by dpalmer          ###   ########.fr       */
+/*   Updated: 2023/04/10 10:59:33 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,9 @@ t_vector	*filename_expansion(char *pattern)
 	t_vector		*matches;
 	struct dirent	*dirent;
 
+	matches = vector_with_capacity(1, sizeof(char *));
+	if (!matches)
+		return (NULL);
 	dirp = opendir(".");
 	if (!dirp)
 	{
